a7282bf2e0a4d02b72db4b7623891152e1c2599c
RybalkoLiliya <156529732+RybalkoLiliya@users.noreply.github.com>
2024-01-28 14:09:06 +0300

    Delete Archive  directory

--- Archive /Archive/Task1_1.py
+++
@@ -1,5 +0,0 @@
-# Task 1-1
-x = float(input())
-y = float(input())
-print(f'Сумма: {x+y} Произведение: {x*y}')
-
--- Archive /Task2_1.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-for i in range(1,10):
-    print(f"{i} x {n} = {i*n:2}")#Форматирование
-
-
