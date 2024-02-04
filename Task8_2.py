f68c83af48be14f952eb25abf777b6f0f399b213
RybalkoLiliya <156529732+RybalkoLiliya@users.noreply.github.com>
2024-02-04 21:15:48 +0300

    Update Task8_2.py

--- Task8_2.py
+++ Task8_2.py
@@ -1,7 +1,6 @@
 def res(lst):
     for sub_lst in lst:
         sub_lst.sort(reverse = True)
-    return sorted(lst, key = len)
+    return sorted(lst, key = lambda x: len((''.join(map(str, x)))))
 
-print(res([[1,3,4,5,6,7], [2,3,4],[5,6,7,1]]))
-
+print(res([[1,3,4,5,6], [26,39,46],[556778,34567]]))
