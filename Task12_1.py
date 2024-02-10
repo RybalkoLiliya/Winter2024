840b30d123812f055c2a6d4314452bdb5b28c5a9
RybalkoLiliya <156529732+RybalkoLiliya@users.noreply.github.com>
2024-02-10 18:16:56 +0300

    Update Task12_1.py

--- Task12_1.py
+++ Task12_1.py
@@ -1,6 +1,8 @@
 lst = [1, 2, 3, 4, 1, 2, 3, 4, 1, 4]
+
 def res(lst):
     lst_min = [i for i in range(len(lst)) if lst[i] == min(lst)]
     lst_max = [i for i in range(len(lst)) if lst[i] == max(lst)]
     return print(f'{lst_min}, {lst_max}')
+    
 res(lst)
