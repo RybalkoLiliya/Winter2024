d976ccb281f69e34bd72988d7f210928c5a0d3a9
RybalkoLiliya <156529732+RybalkoLiliya@users.noreply.github.com>
2024-02-18 18:45:18 +0300

    Update Task15_2.py

--- Task15_2.py
+++ Task15_2.py
@@ -1,6 +1,6 @@
 import re
 def search_number(num):
-    regex = (r'[A, B, E, K, M, H, O, P, C, T, X, Y, А, В, Е, К, М, Н, О, Р, С, Т, Х, У]\d{3}[A, B, E, K, M, H, O, P, C, T, X, Y, А, В, Е, К, М, Н, О, Р, С, Т, Х, У]{2}\d{2,3}')
+    regex = (r'[ABEKMHOPCTXYАВЕКМНОРСТХУ]\d{3}[ABEKMHOPCTXYАВЕКМНОРСТХУ]{2}\d{2,3}')
     res = re.findall(regex, num)
     if res == []:
         return None
