a946ac6d8bdb903441e5dc137381d59cc8681df3
RybalkoLiliya <156529732+RybalkoLiliya@users.noreply.github.com>
2024-02-25 17:20:59 +0300

    Update Task17_3.py

--- Task17_3.py
+++ Task17_3.py
@@ -2,24 +2,24 @@ class Shape:
     def __init__(self, colour, square):
         self.colour = colour
         self.square = square
-    def enter_colour(self):
+    def set_colour(self):
         n1 = input('Установите цвет объекта: ')
         self.colour = n1
-    def what_colour(self):
+    def get_colour(self):
         print(f'{self.colour}')
-    def enter_square(self):
+    def set_square(self):
         n2 = input('Задайте площадь объекта: ')
         self.square = n2
-    def what_square(self):
+    def get_square(self):
         print(f'{self.square}')
 
 a = Shape('Красный', 20)
 b = Shape('Синий', 25)
 
-a.enter_colour()
-a.what_colour()
-b.enter_square()
-b.what_square()
+a.set_colour()
+a.get_colour()
+b.set_square()
+b.get_square()
 
 
 
